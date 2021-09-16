187426908850c56d7cbfc5cd95cf2b2247d0447c
AminSorra <aminelkebirpro@gmail.com>
2021-09-16 16:46:56 +0200

    Form

--- src/Form/DocumentsType.php
+++ src/Form/DocumentsType.php
@@ -6,12 +6,17 @@ use App\Entity\Documents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class DocumentsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('title', TextType::class)
+            
+        ;
         ;
     }
 
